2d2ed1b69baec8887987be6710b2307c460daabc
eguzman2 <edgantonio00@gmail.com>
2024-04-22 16:05:35 -0600

    [ADD] vertical_hospital: create module

---
+++ vertical_hospital/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Vertical Hospital',
+    'summary': 'Patients & Treatments',
+    'sequence': 10,
+    'description': """
+        Patients & Treatments
+        -Manage patients & Treatments.
+        -Generate PDF report
+        -API to share patients data
+    """,
+    'category': 'Specific Industry Applications',
+    'version': '15.0.1.0.0',
+    'depends': [],
+    'data': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
